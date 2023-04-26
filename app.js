c809355ef4bf59cffe4256c5902bf72f4a7fccd4
shemyte <shemyte>
2023-04-26 22:25:27 +0300

    local storage working

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-window.addEventListener('Load', () => {
+window.addEventListener('load', () => {
     todos = JSON.parse(localStorage.getItem('todos')) || [];
     const nameInput = document.querySelector('#name');
-    const newtodoform = document.querySelector('#new-todo-form');
+    const newtodoForm = document.querySelector('#new-todo-form');
     
     const username = localStorage.getItem('username') || '';
     nameInput.value = username;
 
     nameInput.addEventListener('change', e => {
         localStorage.setItem('username', e.target.value);
+    })
+
+    newtodoForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const todo = {
+            content: e.target.elements.content.value,
+            category: e.target.elements.category.value,
+            done: false,
+            createdAt: new Date().getTime() 
+        }
+
+        todos.push(todo);
+        localStorage.setItem('todos', JSON.stringify(todos));
+
+        e.target.reset();
+        
     })
 })
