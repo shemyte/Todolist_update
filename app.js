08770d18451544bfec9c4f6561f6f452b2d0e51b
shemyte <shemyte>
2023-04-26 23:07:06 +0300

    worked on todolists

--- app.js
+++ app.js
@@ -24,6 +24,57 @@ window.addEventListener('load', () => {
         localStorage.setItem('todos', JSON.stringify(todos));
 
         e.target.reset();
-        
+
+        // Displaytodos();
     })
+
+    Displaytodos();
 })
+
+function Displaytodos() {
+    const todoList = document.querySelector('#todo-list');
+
+    todoList.innerHTML = '';
+
+    todos.forEach(todo => {
+        const todoItem = document.createElement('div');
+        todoItem.classList.add('todo-item')
+
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+        const content = document.createElement('div');
+        const actions = document.createElement('div');
+        const edit = document.createElement('button');
+        const deletebutton = document.createElement('button');
+
+        input.type = 'checkbox';
+        input.checked = todo.done;
+        span.classList.add('bubble');
+
+        if (todo.category == 'personal') {
+            span.classList.add('personal');
+        }else {
+            span.classList.add('business');
+        }
+
+        content.classList.add('todo-content');
+        actions.classList.add('actions');
+        edit.classList.add('edit');
+        deletebutton.classList.add('delete');
+
+        content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+        edit.innerHTML = 'Edit';
+        deletebutton.innerHTML = 'Delete';
+
+        label.appendChild(input);
+        label.appendChild(span);
+        actions.appendChild(edit);
+        actions.appendChild(deletebutton);
+        todoItem.appendChild(label);
+        todoItem.appendChild(content);
+        todoItem.appendChild(actions);
+
+        todoList.appendChild(todoItem);
+    })
+}
